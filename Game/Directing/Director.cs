b993f0d2bb1e18dc3b47a9d462109ef3de698744
johnstennett24 <jstennett830@gmail.com>
2022-02-23 13:33:49 -0700

    made the frameowrk for the game

---
+++ Game/Directing/Director.cs
@@ -0,0 +1,35 @@
+using System;
+using Raylib_cs;
+
+
+namespace Snake.Game.Casting
+{
+    public class Director
+    {
+        public Director()
+        {
+        }
+
+        public void StartGame()
+        {
+            GetInputs();
+            DoUpdates();
+            DoOutputs();
+        }
+
+        public void GetInputs()
+        {
+
+        }
+
+        public void DoUpdates()
+        {
+
+        }
+
+        public void DoOutputs()
+        {
+
+        }
+    }
+}
